6e79fb71a484391879ceec76710a25d5f954f0e1
Shyam from Thoon <sundar.vinod@gmail.com>
2017-10-24 02:52:11 +0000

    Split data into separate js file

--- public_html/script.js
+++ public_html/script.js
@@ -1,23 +1,3 @@
-var workExp = [{id: "1", company: "Continental Automotive", role: "Software engineer", period: "Aug 2015 - present", description: ""}, 
-               {id: "2", company: "National University of Singapore", role: "Research engineer", period: "Sep 2012 - Jun 2015", description: ""}, 
-               {id: "3", company: "National University of Singapore", role: "Lab Teaching Assistant", period: "Jun 2010 - Jun 2012", description: ""}, 
-               {id: "4", company: "Sensor Networks and Applications Research Centre", role: "Research Intern", period: "May 2011 - Jul 2011", description: ""}, 
-               {id: "5", company: "Data Storage Institute", role: "Research Intern", period: "May 2010 - Aug 2010", description: ""}];
-
-var education = [{school:"National University of Singapore", subject:"Master of engineering [electrical and computer engineering]", score: "cumulative average point (CAP): 4.75 on 5.00", period: "Jan 2014 - Jul 2015", description: ""}, 
-                 {school:"National University of Singapore", subject:"Bachelor of engineering [computer engineering]", score: "cumulative average point (CAP): 4.24 on 5.00 [2nd upper class honors]", period: "Aug 2008 - Jul 2012", description: ""}];
-
-var projects = [{title: "Whatsapp statistics", description: "View converstation statistics from whatsapp conversations"},
-               {title: "TOI Movie Scraper", description: "Scrape good movie reviews from Times of India and get notified on your phone"},
-               {title: "Carousell notifier", description: "Get notified immediately on items you're looking for on Carousell"},
-{title: "Intel 8088 processor interrupt service", description: "Music memory game playable over two different computers"},
-{title: "Intel 8051 microprocessor", description: "Custom implementation of the data path, ALU, and sequencer of the Intel 8051 microprocessor"},
-{title: "Real-time soccer controller using FPGAs", description: "Real-time AI soccer game using Xilinx Spartan-3A FPGA boards"},
-{title: "Hardware PRESENT block cipher on FPGA", description: "Custom hardware accelerated PRESENT block cipher implementation"},
-{title: "Java encrypted file transfer client", description: "DES encryption based secure file transfer client in Java"},
-{title: "IVLE iOS app", description: "Mobile version of IVLE [student’s education portal] for the iPad"},
-{title: "2D physics engine iPad game", description: "Clone of angry birds game using custom physics engine"}];
-
 function createCardFor(work) {
   var col = document.createElement("div");
   var card = document.createElement("div");
