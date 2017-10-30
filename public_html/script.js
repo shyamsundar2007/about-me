6a240b207702690418ef05661f55bdf47df4a1d7
Shyam <sundar.vinod@gmail.com>
2017-10-30 10:28:49 +0800

    Added info for all projects and centered images

--- public_html/script.js
+++ public_html/script.js
@@ -1,4 +1,4 @@
-imageBasePath = "/assets/images/";
+imageBasePath = "assets/images/";
 
 function createCardFor(work) {
 	var col = document.createElement("div");
@@ -14,16 +14,18 @@ function createCardFor(work) {
 
 	col.className = "col-sm-4";
 	card.className = "card mt-2 mb-2";
-	img.className = "card-img-top img-fluid";
+	img.className = "card-img-top img-fluid rounded mx-auto d-block";
 	img.src = imageBasePath + work.image;
 	body.className = "card-body";
 	title.className = "card-title";
 	subtitle.className = "card-subtitle";
 	text.className = "card-text";
+	
 	expand.setAttribute("data-toggle", "collapse");
-	expand.href = "expand" + work.id;
+	expand.setAttribute("style", "text-decoration:none");
+	expand.href = "expandWork" + work.id;
 	expandDiv.className = "collapse";
-	expandDiv.id = "expand" + work.id;
+	expandDiv.id = "expandWork" + work.id;
 
 	// add description as unordered list
 	for (index in work.description) {
@@ -34,7 +36,7 @@ function createCardFor(work) {
 
 	expandDiv.appendChild(expandDesc);
 	expand.onclick = function () {
-		$('#expand' + work.id).collapse('toggle');
+		$('#expandWork' + work.id).collapse('toggle');
 	}
 	
 	title.innerHTML = work.company;
@@ -44,10 +46,10 @@ function createCardFor(work) {
 	body.appendChild(subtitle);
 	body.appendChild(text);
 	body.appendChild(expandDiv);
-	card.appendChild(img);
-	card.appendChild(body);
-	expand.appendChild(card);
-	col.appendChild(expand);
+	expand.appendChild(img);
+	card.appendChild(expand);
+	card.appendChild(body);	
+	col.appendChild(card);
 	return col;
 }
 
@@ -59,9 +61,13 @@ function createCardForSchool(school) {
 	var title = document.createElement("h4");
 	var subtitle = document.createElement("h6");
 	var text = document.createElement("p");
+	var expand = document.createElement("a");
+	var expandDiv = document.createElement("div");
+	var expandDesc = document.createElement("ul");
+	
 	col.className = "col-sm-4";
 	card.className = "card mt-2 mb-2";
-	img.className = "card-img-top img-fluid";
+	img.className = "card-img-top img-fluid rounded mx-auto d-block";
 	img.src = imageBasePath + school.image;
 	body.className = "card-body";
 	title.className = "card-title";
@@ -70,11 +76,32 @@ function createCardForSchool(school) {
 	title.innerHTML = school.school;
 	subtitle.innerHTML = school.subject;
 	text.innerHTML = school.period;
+	
+	expand.setAttribute("data-toggle", "collapse");
+	expand.setAttribute("style", "text-decoration:none");
+	expand.href = "expandSchool" + school.id;
+	expandDiv.className = "collapse";
+	expandDiv.id = "expandSchool" + school.id;
+	
+	// add description as unordered list
+	for (index in school.description) {
+		var item = document.createElement("li");
+		item.innerHTML = school.description[index];
+		expandDesc.appendChild(item);
+	}
+	
+	expandDiv.appendChild(expandDesc);
+	expand.onclick = function () {
+		$('#expandSchool' + school.id).collapse('toggle');
+	}
+	
 	body.appendChild(title);
 	body.appendChild(subtitle);
 	body.appendChild(text);
-	card.appendChild(img);
-	card.appendChild(body);
+	body.appendChild(expandDiv);
+	expand.appendChild(img);
+	card.appendChild(expand);
+	card.appendChild(body);	
 	col.appendChild(card);
 	return col;
 }
@@ -86,19 +113,45 @@ function createCardForProject(project) {
 	var body = document.createElement("div");
 	var title = document.createElement("h4");
 	var text = document.createElement("p");
+	var expand = document.createElement("a");
+	var expandDiv = document.createElement("div");
+	var expandDesc = document.createElement("ul");
+	
 	col.className = "col-sm-4";
 	card.className = "card mt-2 mb-2";
-	img.className = "card-img-top img-fluid";
+	img.className = "card-img-top img-fluid rounded mx-auto d-block";
 	img.src = imageBasePath + project.image;
 	body.className = "card-body";
 	title.className = "card-title";
 	text.className = "card-text";
 	title.innerHTML = project.title;
 	text.innerHTML = project.description;
+	expand.setAttribute("data-toggle", "collapse");
+	expand.setAttribute("style", "text-decoration:none");
+	expand.href = "expandProject" + project.id;
+	expandDiv.className = "collapse";
+	expandDiv.id = "expandProject" + project.id;
+	
+	// add description as unordered list
+	if(project.hasOwnProperty("additionalInfo")) {
+		for (index in project.additionalInfo) {
+			var item = document.createElement("li");
+			item.innerHTML = project.additionalInfo[index];
+			expandDesc.appendChild(item);
+		}
+	}
+	
+	expandDiv.appendChild(expandDesc);
+	expand.onclick = function () {
+		$('#expandProject' + project.id).collapse('toggle');
+	}
+	
 	body.appendChild(title);
 	body.appendChild(text);
-	card.appendChild(img);
-	card.appendChild(body);
+	body.appendChild(expandDiv);
+	expand.appendChild(img);
+	card.appendChild(expand);
+	card.appendChild(body);	
 	col.appendChild(card);
 	return col;
 }
